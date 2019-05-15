0e43aa813b0cb0e01113137705ea7bcefc6ee535
wantyouring <pwc99@naver.com>
2019-05-15 14:59:05 +0900

    using intent2. tutorial12-2

--- Tutorial12/app/src/main/java/com/example/tutorial12/MainActivity.java
+++ Tutorial12/app/src/main/java/com/example/tutorial12/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.tutorial12;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,11 +31,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(v.getContext(),NextActivity.class);
                 String login_id = loginText.getText().toString();
                 intent.putExtra("login_id",login_id);
-                startActivity(intent);
+                startActivityForResult(intent,5); //request코드 함께 보냄
             }
         });
-
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 5 && resultCode == 10) {
+            TextView logout_text = (TextView)findViewById(R.id.logoutText);
+            logout_text.setText(data.getStringExtra("logout_id")+ "님이 로그아웃 하였습니다.");
+        }
+    }
+
 }
--- Tutorial12/app/src/main/java/com/example/tutorial12/NextActivity.java
+++ Tutorial12/app/src/main/java/com/example/tutorial12/NextActivity.java
@@ -3,6 +3,8 @@ package com.example.tutorial12;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NextActivity extends AppCompatActivity {
@@ -12,7 +14,18 @@ public class NextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        TextView user_name = (TextView)findViewById(R.id.user_name);
+        final TextView user_name = (TextView)findViewById(R.id.user_name);
         user_name.setText(getIntent().getStringExtra("login_id") + "님");
+
+        Button exitButton = (Button)findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),MainActivity.class);
+                intent.putExtra("logout_id",user_name.getText().toString());
+                setResult(10,intent);
+                finish();
+            }
+        });
     }
 }
