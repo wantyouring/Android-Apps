7ba4737bdb92ceaa74d2804b60c597e269aa4cb0
wantyouring <pwc99@naver.com>
2019-05-16 00:31:57 +0900

    notification. implements button listener.
    tutorial14

--- Tutorial14/app/src/main/java/com/example/tutorial14/MainActivity.java
+++ Tutorial14/app/src/main/java/com/example/tutorial14/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.tutorial14;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View .OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                notification1();
+                break;
+        }
+    }
+
+    //일반 알림
+    private void notification1() {
+        // API level 26부터 channelid추가되어 설정 필요. (from stackoverflow)
+        initChannels(this);
+        //알림창 정보 set
+        Intent intent = new Intent(this, MainActivity.class); //open할 activity
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"default")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("알림 테스트")
+                .setContentText("원츄우우우우링링링링")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
+
+        // 알림 띄우기
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build()); // notificationId is a unique int for each notification that you must define
+
+    }
+
+    // API level따라서 채널 설정
+    public void initChannels(Context context) {
+        if (Build.VERSION.SDK_INT < 26) {
+            return;
+        }
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("default",
+                "Channel name",
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Channel description");
+        notificationManager.createNotificationChannel(channel);
     }
 }
