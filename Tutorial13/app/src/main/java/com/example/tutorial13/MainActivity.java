66e811773184423e61264446281bfdd709c97216
wantyouring <pwc99@naver.com>
2019-05-15 21:19:48 +0900

    take picture. save gallery. get picture from gallery.
    tutorial13-3

--- Tutorial13/app/src/main/java/com/example/tutorial13/MainActivity.java
+++ Tutorial13/app/src/main/java/com/example/tutorial13/MainActivity.java
@@ -26,6 +26,8 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CAMERA_CODE = 1;
+    private static final int GALLERY_CODE = 2;
     String currentPhotoPath;
 
     @Override
@@ -35,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
         // 처음 앱 실행했을 시 권한 요청하는 부분
         requirePermission();
+
+        Button galleryButton = (Button)findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickUpPicture();
+            }
+        });
+
+        Button saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                galleryAddPic();
+            }
+        });
 
         Button button = (Button)findViewById(R.id.camera_button);
         //사진찍기 클릭 시 해당 권한 확인
@@ -73,24 +91,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 사진찍기
     void takePicture() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
             File photoFile = createImageFile();
             Uri photoUri = FileProvider.getUriForFile(this, "com.example.tutorial13.fileprovider", photoFile );
             intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
-            startActivityForResult(intent,5); //어디서 보냈는지 filtering
+            startActivityForResult(intent,CAMERA_CODE); //어디서 보냈는지 filtering
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
+    // 이미지파일 생성.
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM); //앱 내부저장소 접근불가. 외부저장소 DCIM에 저장.
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -102,12 +122,37 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
-    //사진찍기 버튼 눌렀을 시
+    // intent에서 requestCode받았을 시 동작.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 5) {
+        //사진찍기 찍었을시
+        if(requestCode == CAMERA_CODE) {
             ImageView imageView = (ImageView)findViewById(R.id.imageView);
             imageView.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath));
         }
+
+        if(requestCode == GALLERY_CODE) {
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView2);
+            Uri uri = data.getData();
+            imageView2.setImageURI(uri);
+        }
+    }
+
+    // 갤러리에 사진 추가
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        Toast.makeText(this, "사진이 저장되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    // 갤러리에서 사진 가져오기
+    void pickUpPicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent,GALLERY_CODE);
     }
 }
