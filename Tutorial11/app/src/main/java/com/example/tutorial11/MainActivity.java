67157ac49854fc3691ccb1bdb741698880e107ff
wantyouring <pwc99@naver.com>
2019-05-15 12:46:34 +0900

    tool bar.

--- Tutorial11/app/src/main/java/com/example/tutorial11/MainActivity.java
+++ Tutorial11/app/src/main/java/com/example/tutorial11/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.tutorial11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageButton imageButton = (ImageButton)findViewById(R.id.toolbar_email);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "도착한 메일이 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
