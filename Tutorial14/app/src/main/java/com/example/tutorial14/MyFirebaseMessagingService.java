a277e39d01080a868adcb7d9cdde148178a00fca
wantyouring <pwc99@naver.com>
2019-05-16 02:09:50 +0900

    14-2 push notification with firebase.
    excepted google-services.json file. add json file to Tutorial14/app folder when use.

--- Tutorial14/app/src/main/java/com/example/tutorial14/MyFirebaseMessagingService.java
+++ Tutorial14/app/src/main/java/com/example/tutorial14/MyFirebaseMessagingService.java
@@ -8,33 +8,21 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View .OnClickListener{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-        Button button1 = (Button)findViewById(R.id.button1);
-        button1.setOnClickListener(this);
-    }
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button1:
-                notification1();
-                break;
-        }
-    }
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // 포그라운드에서만 작동함
+        //Log.d("Firebase", "From: " + remoteMessage.getFrom());
+        //Log.d("Firebase", "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
-    //일반 알림
-    private void notification1() {
-        // API level 26부터 channelid추가되어 설정 필요. (from stackoverflow)
+        //String from = remoteMessage.getFrom();
+        String bodyFromServer = remoteMessage.getNotification().getBody();
+
         initChannels(this);
         //알림창 정보 set
         Intent intent = new Intent(this, MainActivity.class); //open할 activity
@@ -42,8 +30,8 @@ public class MainActivity extends AppCompatActivity implements View .OnClickList
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"default")
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("알림 테스트")
-                .setContentText("원츄우우우우링링링링")
+                .setContentTitle("푸시알림")
+                .setContentText(bodyFromServer)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent);
 
@@ -52,7 +40,6 @@ public class MainActivity extends AppCompatActivity implements View .OnClickList
         notificationManager.notify(0, builder.build()); // notificationId is a unique int for each notification that you must define
 
     }
-
     // API level따라서 채널 설정
     public void initChannels(Context context) {
         if (Build.VERSION.SDK_INT < 26) {
@@ -66,5 +53,4 @@ public class MainActivity extends AppCompatActivity implements View .OnClickList
         channel.setDescription("Channel description");
         notificationManager.createNotificationChannel(channel);
     }
-
 }
